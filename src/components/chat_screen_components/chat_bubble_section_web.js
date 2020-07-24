4819bfdcae7e3347e3896a30c1e8fb11241dccb9
Laurell Seville <laurellseville2000@gmail.com>
2020-07-23 21:29:47 -0500

    chat screen done for web

--- src/components/chat_screen_components/chat_bubble_section_web.js
+++ src/components/chat_screen_components/chat_bubble_section_web.js
@@ -6,7 +6,45 @@ export default function ChatBubbleSectionWeb() {
         <div id="bubble_section_heading">
             Elizabeth Williams
         </div>
-        <div id="bubbles"></div>
+        <div id="bubbles">
+
+            <div id="message_received_bubble_section">
+                <div id="received_bubble">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque egestas diam est. Nullam auctor nibh velit. Donec facilisis sodales metus, et pharetra erat sollicitudin ut.
+                </div>
+            </div>
+
+            <div id="message_sent_bubble_section">
+                <div id="sent_bubble">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque egestas diam est. Nullam auctor nibh velit. Donec facilisis sodales metus, et pharetra erat sollicitudin ut.
+                </div>
+            </div>
+            <div id="message_received_bubble_section">
+                <div id="received_bubble">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque egestas diam est. Nullam auctor nibh velit. Donec facilisis sodales metus, et pharetra erat sollicitudin ut.
+                </div>
+            </div>
+
+            <div id="message_sent_bubble_section">
+                <div id="sent_bubble">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque egestas diam est. Nullam auctor nibh velit. Donec facilisis sodales metus, et pharetra erat sollicitudin ut.
+                </div>
+            </div>
+            <div id="message_received_bubble_section">
+                <div id="received_bubble">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque egestas diam est. Nullam auctor nibh velit. Donec facilisis sodales metus, et pharetra erat sollicitudin ut.
+                </div>
+            </div>
+
+            <div id="message_sent_bubble_section">
+                <div id="sent_bubble">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque egestas diam est. Nullam auctor nibh velit. Donec facilisis sodales metus, et pharetra erat sollicitudin ut.
+                </div>
+            </div>
+
+        </div>
+
+
         <div id="send_message_section">
             <input id="send_message_input" type="text" placeholder="Write A Message" />
             <IconButton id="icon_button" color="inherit">
