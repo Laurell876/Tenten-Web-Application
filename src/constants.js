b45697615a625b631b5c3048cfe65f91daad4118
Laurell Seville <laurellseville2000@gmail.com>
2020-08-16 12:35:18 -0500

    sub url is secure

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 
 export const URI = 'https://tenten-graphql-api.herokuapp.com/';
 
-export const SUBURI ='ws://tenten-graphql-api.herokuapp.com/'
+export const SUBURI ='wss://tenten-graphql-api.herokuapp.com/'
