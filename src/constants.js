dea22a28e12f96d57f1f330741e1932cbaa55564
Laurell Seville <laurellseville2000@gmail.com>
2020-11-26 21:05:14 -0500

    rebuilt sass modules

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 
-export const URI = 'https://tenten-graphql-api.herokuapp.com/';
+export const URI = 'http://localhost:4000/';
 
-export const SUBURI ='wss://tenten-graphql-api.herokuapp.com/'
+export const SUBURI ='ws://localhost:4000/';
