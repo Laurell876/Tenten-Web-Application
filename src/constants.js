c482a74b66137857db44ca95ef0abdf8a2a568e9
Laurell Seville <laurellseville2000@gmail.com>
2020-12-03 20:43:29 -0500

    changed urls

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 
-export const URI = 'http://localhost:4000/';
+export const URI = 'https://tenten-graphql-api.herokuapp.com/';
 
-export const SUBURI ='ws://localhost:4000/';
+export const SUBURI ='ws://tenten-graphql-api.herokuapp.com/';
